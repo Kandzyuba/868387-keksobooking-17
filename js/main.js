3d3933f8a2dbc38f4bc0f9fe200a5e5da5dd4a02
KaNiKir <kandzyuba@gmail.com>
2019-06-26 23:49:40 +0300

    Выполнено задание - максимум подвижности

--- js/main.js
+++ js/main.js
@@ -123,12 +123,8 @@ var activePage = function () {
   renderPins(similarListElement, fragment);
 };
 
-pinMain.addEventListener('click', function () {
-  activePage();
-});
-
 pinMain.addEventListener('mouseup', function () {
-  addAdressValue(pinPosition.x, pinPosition.y);
+
 });
 
 // Валидация формы
@@ -164,4 +160,54 @@ timeout.addEventListener('change', function () {
   var timeoutIndex = timeout.options.selectedIndex;
 
   time(timein, timeoutIndex);
+});
+
+// перемещение диалога
+pinMain.addEventListener('mousedown', function (evt) {
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  activePage();
+
+  var onMouseMove = function (moveEvt) {
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    var positionY = pinMain.offsetTop - shift.y;
+    var positionX = pinMain.offsetLeft - shift.x;
+
+    var OFFSET_X_MAX = 1135;
+    var OFFSET_Y_MIN = 130;
+    var OFFSET_Y_MAX = 630;
+
+    positionX = positionX < 0 ? 0 : positionX;
+    positionX = positionX > OFFSET_X_MAX ? OFFSET_X_MAX : positionX;
+
+    positionY = positionY < OFFSET_Y_MIN ? OFFSET_Y_MIN : positionY;
+    positionY = positionY > OFFSET_Y_MAX ? OFFSET_Y_MAX : positionY;
+
+    addAdressValue(positionX, positionY);
+
+    pinMain.style.top = positionY + 'px';
+    pinMain.style.left = positionX + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 });
