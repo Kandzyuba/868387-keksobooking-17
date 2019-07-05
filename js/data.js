e640acc7f23e52a5e78bf54b9b5855f16027a632
KaNiKir <kandzyuba@gmail.com>
2019-07-05 21:31:10 +0300

    создан модуль отвечающий за фильтры

--- js/data.js
+++ js/data.js
@@ -62,27 +62,4 @@
 
     evt.preventDefault();
   });
-
-  // Фильтрация по типу жилья/стоимости/числа комнат/числа гостей/услуг
-  window.mapFilters = document.querySelector('.map__filters');
-
-  var housingFilter = function (data) {
-    var housingType = document.querySelector('#housing-type');
-    var selectedHouse = housingType.options.selectedIndex;
-
-    var houseArr = data.filter(function (elem) {
-      if (housingType.value === 'any') {
-        return elem.offer.type;
-      }
-      return elem.offer.type === housingType.options[selectedHouse].value;
-    });
-    return houseArr;
-  };
-
-  window.generalFilter = function () {
-    window.removePins();
-    window.renderPins(housingFilter(window.dataCard));
-  };
-
-  window.mapFilters.addEventListener('change', window.generalFilter);
 })();
